39bc1a0cae12cb918ac7e23483651b563e770d23
Sathwik M S <sathvikms344@gmail.com>
2025-08-15 19:07:11 +0530

    project section completed

--- app.js
+++ app.js
@@ -8,3 +8,24 @@ document.getElementById("btn-cv").addEventListener("click",() =>{
 document.getElementById("btn-con").addEventListener("click",()=>{
     location.href='./#contact';
 })
+
+//project1 btn view
+document.getElementById("one-view").addEventListener("click",() =>{
+    window.open("https://niranjansimha.netlify.app/",'_blank');
+})
+
+//project1 btn github
+
+document.getElementById("one-src").addEventListener("click",() =>{
+    window.open("",'_blank');
+})
+
+// project2 btn view
+document.getElementById("two-view").addEventListener("click",() =>{
+    window.open("https://sathwikms.netlify.app/",'_blank');
+})
+
+//project2 btn github
+document.getElementById("two-src").addEventListener("click",() =>{
+    window.open("https://github.com/codewithsathwik/my-portfolio",'_blank');
+})
